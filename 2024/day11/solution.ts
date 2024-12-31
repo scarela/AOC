f066087a2af2ebdafa31af152ff4ae363eb38afb
Shamil Carela <sha.carela@gmail.com>
2024-12-30 20:34:18 -0400

    Add 2024 day 11 part 2 counting occurrences instead of brute force and optimized part 1

--- 2024/day11/solution.ts
+++ 2024/day11/solution.ts
@@ -4,33 +4,65 @@ const data = await file(resolveSync("./input", import.meta.dir)).text();
 // const data = await file(resolveSync("./input.test", import.meta.dir)).text();
 
 function part1() {
-  let index = 0;
-  const steps = 25;
-  const numbers = data.match(/\d+/g)?.map(Number) ?? [];
+  const blinks = 25;
+  let numbers = data.match(/\d+/g)?.reduce((acc: Record<string, number>, k) => {
+    acc[k] = (acc[k] ?? 0) + 1;
+    return acc;
+  }, {}) ?? {};
 
-  while (index < steps) {
-    for (let i = 0; i < numbers.length; i++) {
-      if (numbers[i] === 0) {
-        numbers[i] = 1;
-      } else if (String(numbers[i]).length % 2 === 0) {
-        const str = String(numbers[i]);
-        numbers[i] = +str.slice(0, str.length / 2);
-        numbers.splice(i + 1, 0, +str.slice(str.length / 2))
-        i++;
+  for (let i = 1; i <= blinks; i++) {
+    const current: Record<string, number> = {};
+
+    for (const [k, v] of Object.entries(numbers)) {
+      if (k === '0') {
+        current["1"] = v;
+      } else if (k.length % 2 === 0) {
+        const [a, b] = [+k.slice(0, k.length / 2), +k.slice(k.length / 2)];
+        current[a] = (current[a] ?? 0) + v;
+        current[b] = (current[b] ?? 0) + v;
       } else {
-        numbers[i] *= 2024;
+        const str = +k * 2024;
+        current[str] = v;
       }
     }
-    index++;
+
+    numbers = { ...current };
   }
 
-  return numbers.length;
+  return Object.values(numbers).reduce((acc, v) => acc + v, 0);
 }
 
 console.log("Day 11 - Part 1:", part1());
 
 function part2() {
-  return 0
+  let blinks = 75;
+  let index = 1;
+  let numbers = data.match(/\d+/g)?.reduce((acc: Record<string, number>, n) => {
+    acc[n] = acc[n] ?? 0 + 1;
+    return acc;
+  }, {}) ?? {};
+
+  while (index <= blinks) {
+    const current: Record<string, number> = {};
+    Object.entries(numbers)
+      .forEach(([k, v]) => {
+
+        if (k === '0') {
+          current["1"] = v;
+        } else if (k.length % 2 === 0) {
+          const [a, b] = [k.slice(0, k.length / 2), k.slice(k.length / 2)];
+          current[+a] = (current[+a] ?? 0) + v;
+          current[+b] = (current[+b] ?? 0) + v;
+        } else {
+          const str = +k * 2024;
+          current[str] = v;
+        }
+      });
+    index++;
+    numbers = { ...current };
+  }
+
+  return Object.values(numbers).reduce((acc, n) => acc + n, 0);
 }
 
 console.log("Day 11 - Part 2:", part2());
