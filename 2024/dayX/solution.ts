bb4919b810a322fba6b1bb9750622cf5f4affb70
Shamil Carela <sha.carela@gmail.com>
2024-12-30 20:56:51 -0400

    Add Bun day template

---
+++ 2024/dayX/solution.ts
@@ -0,0 +1,17 @@
+import { file, resolveSync } from "bun";
+
+// const data = await file(resolveSync("./input", import.meta.dir)).text();
+const data = await file(resolveSync("./input.test", import.meta.dir)).text();
+
+function part1() {
+  return 0;
+}
+
+console.log("Day XX - Part 1:", part1());
+
+function part2() {
+
+  return 0;
+}
+
+console.log("Day XX - Part 2:", part2());
