8dbdb203b1756904f97ab37c07df0936a2b8ca66
Shamil Carela <sha.carela@gmail.com>
2025-01-04 22:05:50 -0400

    Add 2023 day 10 part 2 using the shoelace formula and prick's theorem

--- 2023/day10/solution.ts
+++ 2023/day10/solution.ts
@@ -7,27 +7,23 @@ const grid = data.split('\n').map(l => [...l])
 const dir = [[-1, 0], [0, 1], [1, 0], [0, -1]]
 
 const dirMap = new Map<string, number>([
-  [['|', 0], ['F', 1], ['7', 3]], // going up
-  [['J', 0], ['-', 1], ['7', 2]], // going right
-  [['L', 1], ['|', 2], ['J', 3]], // going down
-  [['L', 0], ['F', 2], ['-', 3]] // going left
+  [['|', 0], ['F', 1], ['7', 3]], // if up: go up, left or right
+  [['J', 0], ['-', 1], ['7', 2]], // if right: go up right, or down
+  [['L', 1], ['|', 2], ['J', 3]], // if down: go right, down or left
+  [['L', 0], ['F', 2], ['-', 3]] // if left: go up, or down or left
 ].flatMap((arr, i) => [...arr].map(([symbol, j]) => [symbol + "" + i, j as number])))
-
-const withinLimits = ([h, w]: [number, number], point: number[]) => {
-  const [row, col,] = point;
-  return;
-}
 
 const getInitial = (grid: string[][], animal_symbol: string) => {
   const h = grid.length;
   const w = grid[0].length;
   let p1: number[];
   let p2: number[];
+  let start: number[];
 
   for (let row = 0; row < h; row++) {
     for (let col = 0; col < w; col++) {
       if (grid[row][col] === animal_symbol) {
-
+        start = [row, col];
         [p1, p2] = dir
           .map(([dr, dc], i) => [row + dr, col + dc, i])
           .filter(([row, col,]) => row >= 0 && col >= 0 && row < h && col < w)
@@ -39,14 +35,14 @@ const getInitial = (grid: string[][], animal_symbol: string) => {
     }
   }
 
-  return [p1!, p2!] as [number[], number[]];
+  return [p1!, p2!, start!] as [number[], number[], number[]];
 }
 
 const coord = (arr: number[]) => arr.slice(0, 2).join('_')
 const moveToDir = (p: number[], to: number) => [p[0] + dir[to][0], p[1] + dir[to][1], to];
 
 function part1() {
-  let [p1, p2] = getInitial(grid, 'S');
+  let [p1, p2,] = getInitial(grid, 'S');
   let count = 1;
 
   while (coord(p1) !== coord(p2)) {
@@ -65,8 +61,43 @@ function part1() {
 console.log("Day 10 - Part 1:", part1());
 
 function part2() {
+  let [p1, p2, start] = getInitial(grid, 'S');
+  const counterClock: number[][] = [start];
+  const clockWise: number[][] = [];
 
-  return 0;
+  while (true) {
+    counterClock.push(p2);
+    clockWise.push(p1);
+    let [p1r, p1c, p1dir] = p1;
+    let [p2r, p2c, p2dir] = p2;
+
+    p1 = moveToDir(p1, dirMap.get(grid[p1r][p1c] + p1dir)!)
+    p2 = moveToDir(p2, dirMap.get(grid[p2r][p2c] + p2dir)!)
+
+    if (coord(p1) === coord(p2)) {
+      counterClock.push(p2)
+      break;
+    }
+  }
+
+  return shoeLaceAndPrick(counterClock.concat(clockWise.toReversed()));
+}
+
+const shoeLaceAndPrick = (points: number[][]) => {
+  let result = 0;
+  let length = points.length;
+  points.push(points[0]);
+
+  for (let i = 1; i < points.length; i++) {
+    const [y1, x1] = points[i - 1];
+    const [y2, x2] = points[i];
+
+    result += (x1 * y2) - (y1 * x2)
+  }
+
+  let area = Math.abs(result / 2); //Don't know why I'm getting neg numbers
+
+  return area - (length / 2) + 1;
 }
 
 console.log("Day 10 - Part 2:", part2());
